d56daad8a0585f10d3fec8072e34c998e02717d8
Adam Malos <adammalos@DESKTOP-4TD58B9>
2023-06-16 14:25:40 -0300

    Arquivos com testes automático e manual

--- botTeste.js
+++ botTeste.js
@@ -1,12 +1,11 @@
-const prompt = require('prompt-sync')();
 const nodemailer = require('nodemailer');
 
 // Configurações do serviço de e-mail
 const transporter = nodemailer.createTransport({
   service: 'hotmail',
   auth: {
-    user: 'enviadasenha@outlook.com',
-    pass: 'W7bp!9e7L%WS'
+    user: 'senharec0@outlook.com',
+    pass: 'S0Ks9eWIlx8@'
   }
 });
 
@@ -27,7 +26,7 @@ function gerarSenha() {
 // Função para enviar o e-mail com a nova senha
 async function enviarEmailNovaSenha(email, novaSenha) {
   const mensagem = {
-    from: 'enviadasenha@outlook.com',
+    from: 'senharec0@outlook.com',
     to: email,
     subject: 'Nova senha gerada',
     text: `Sua nova senha é: ${novaSenha}`
@@ -43,42 +42,49 @@ async function enviarEmailNovaSenha(email, novaSenha) {
 
 // Função principal do sistema de recuperação de senha
 async function recuperarSenha() {
-  const senhaInserida = prompt('Insira a senha: ');
-
+  let senhaInserida = 'admin1234'; // Senha correta
+  // Teste 1: Senha correta
   if (senhaInserida === senhaCadastrada) {
     console.log('Senha correta. Acesso permitido.');
   } else {
     console.log('Senha incorreta.');
+  }
 
-    const criarNovaSenha = prompt('Deseja criar uma nova senha? (sim/não): ');
+  // Teste 2: Senha errada sem gerar nova senha
+  senhaInserida = 'senhaerrada';
+  if (senhaInserida !== senhaCadastrada) {
+    console.log('Senha incorreta.');
 
+    // Teste 3: Senha errada com nova senha gerada
+    const criarNovaSenha = 'sim';
     if (criarNovaSenha.toLowerCase() === 'sim') {
       const novaSenha = gerarSenha();
       senhaCadastrada = novaSenha;
       console.log('Nova senha gerada:', novaSenha);
 
-      await enviarEmailNovaSenha('admin@email.com', novaSenha);
+      await enviarEmailNovaSenha('admin@example.com', novaSenha);
 
       setTimeout(async function() {
         console.log('Acesso negado. O tempo para a nova senha expirou.');
 
-        const enviarNovamente = prompt('Deseja enviar um novo e-mail com a senha? (sim/não): ');
+        // Teste 4: Senha errada sem usar nova senha gerada
+        const enviarNovamente = 'nao';
         if (enviarNovamente.toLowerCase() === 'sim') {
           const novaSenha = gerarSenha();
           senhaCadastrada = novaSenha;
           console.log('Nova senha gerada:', novaSenha);
 
-          await enviarEmailNovaSenha(novaSenha);
+          await enviarEmailNovaSenha('admin@example.com', novaSenha);
           await recuperarSenha();
         } else {
           console.log('Acesso negado.');
         }
-      }, 600000); // 10 segundos
+      }, 10000); // 10 segundos
 
       console.log('Aguarde o e-mail com a nova senha...');
-      const senhaInserida = prompt('Insira a nova senha recebida por e-mail: ');
+      const senhaInseridaNovamente = novaSenha; // Usar nova senha gerada para os testes
 
-      if (senhaInserida === novaSenha) {
+      if (senhaInseridaNovamente === novaSenha) {
         console.log('Senha correta. Acesso permitido.');
       } else {
         console.log('Senha incorreta. Acesso negado.');
@@ -90,4 +96,10 @@ async function recuperarSenha() {
 }
 
 // Iniciar o sistema de recuperação de senha
-recuperarSenha();
+recuperarSenha()
+  .then(() => {
+    console.log('Teste concluído.');
+  })
+  .catch((error) => {
+    console.log('Ocorreu um erro:', error);
+  });
