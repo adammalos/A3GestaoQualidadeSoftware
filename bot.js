98a2da79ad4b06286dbdbdf0f153d47d84c3edaf
Adam Malos <adammalos@DESKTOP-4TD58B9>
2023-06-15 22:13:15 -0300

    Validação de senha

---
+++ bot.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')();
+
+// Função para gerar uma senha aleatória
+function gerarSenha() {
+  var caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var senha = '';
+  for (var i = 0; i < 8; i++) {
+    var indice = Math.floor(Math.random() * caracteres.length);
+    senha += caracteres.charAt(indice);
+  }
+  return senha;
+}
+
+// Função principal do bot
+function bot() {
+  var senhaGerada = gerarSenha();
+  console.log('Senha gerada:', senhaGerada);
+
+  var senhaInserida = prompt('Insira a senha: ');
+
+  if (senhaInserida === senhaGerada) {
+    console.log('OK');
+  } else {
+    console.log('Error');
+  }
+}
+
+// Iniciar o bot
+bot();
